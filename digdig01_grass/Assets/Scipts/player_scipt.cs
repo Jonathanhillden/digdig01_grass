846e7e67fee68cb259d7fcbbcfe930b9833d06b7
Jonathanhillden <74252905+Jonathanhillden@users.noreply.github.com>
2021-02-02 15:55:17 +0100

    Player movement + jump
    Spelaren kan röra sig och hoppa

--- digdig01_grass/Assets/Scipts/player_scipt.cs
+++ digdig01_grass/Assets/Scipts/player_scipt.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class player_scipt : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public float jump = 5f;
+    public bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class player_scipt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+        }
     }
 }

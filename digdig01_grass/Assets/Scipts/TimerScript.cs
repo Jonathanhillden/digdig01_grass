d34d3a1729a215faad6d0db4eb6fd8dea6e89126
Jonathanhillden <74252905+Jonathanhillden@users.noreply.github.com>
2021-02-09 15:57:22 +0100

    Added working timer
    Very good working timer that we need to change later.

---
+++ digdig01_grass/Assets/Scipts/TimerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+
+    public static float timerValue = 0;
+    Text timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerValue += 1 * Time.deltaTime;
+        timer.text = "Time: " + timerValue.ToString ("0");
+    }
+
+}

d2a07e39023137adb890165e048dbe057137331a
Jonathanhillden <74252905+Jonathanhillden@users.noreply.github.com>
2021-02-23 15:47:17 +0100

    Player attack
    En attack som gör 1 i skada. Skapade ett script till spelaren, ett till fienden och et nytt layer (nr 8) som fienden behöver ha.

---
+++ digdig01_grass/Assets/Scipts/enemyHP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class enemyHP : MonoBehaviour
+{
+    public int maxHealth = 1;
+    private int currentHealth; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage; 
+        //Hurt animation (if more than 1 HP)
+        if (currentHealth <= 0)
+        {
+            Die(); 
+        }
+    }
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+        Destroy(gameObject);
+    }
+}
---
+++ digdig01_grass/Assets/Scipts/playerCombat.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCombat : MonoBehaviour
+{
+    public Transform attackArea;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+
+    //Cooldown
+    public float attackRate = 2f;
+    float nextAttackTime = 0;
+
+    public int attackDamage = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate; 
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //Animation
+        //Detect enemy in range
+        Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(attackArea.position, attackRange, enemyLayer); 
+        //Apply damage to enemy
+        foreach (Collider2D enemy in hitEnemy)
+        {
+            enemy.GetComponent<enemyHP>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackArea == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackArea.position, attackRange);
+    }
+}

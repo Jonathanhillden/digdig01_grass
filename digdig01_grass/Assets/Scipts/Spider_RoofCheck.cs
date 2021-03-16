746b8fcb1698f8be0a7d402a6b24ebffc56972ba
Jonathanhillden <74252905+Jonathanhillden@users.noreply.github.com>
2021-03-16 16:11:28 +0100

    Spider Update
    Jobbar vidare på spindeln

---
+++ digdig01_grass/Assets/Scipts/Spider_RoofCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider_RoofCheck : MonoBehaviour
+{
+    GameObject Spider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spider = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Spider.GetComponent<spiderScript>().Under = true;
+    }
+}
--- digdig01_grass/Assets/Scipts/spiderScript.cs
+++ digdig01_grass/Assets/Scipts/spiderScript.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 
 public class spiderScript : MonoBehaviour
 {
-    public bool groundCheck = false;
     public float moveSpeed = 3f;
+    Rigidbody2D spiderRigidbody;
+    public bool onTop = false;
+    public bool onRightWall = false;
+    public bool onLeftWall = false;
+    public bool Under = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spiderRigidbody = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move(); 
-    }
-    private void Move()
-    {
-        if (groundCheck == true)
+        /*if (facingLeft())
         {
-            //transform.position +=
+            spiderRigidbody.velocity = new Vector2(-moveSpeed, 0f);
+        }*/
+        Top();
+        isUnder();
+    }
+    /*private bool facingLeft()
+    {
+        return transform.localScale.x > Mathf.Epsilon; 
+    }*/
+
+    private void Top()
+    {
+        if (onTop == true)
+        {
+            //transform.Rotate(0f, 90f, 0f); 
+            spiderRigidbody.velocity = new Vector2(-moveSpeed, 0f);
         }
     }
+
+    private void isUnder()
+    {
+        
+    }
+    
+    
 }

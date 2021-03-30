15add89bcf56b03c24ac4f4f9b19ccf915edbc7a
Jonathanhillden <74252905+Jonathanhillden@users.noreply.github.com>
2021-03-30 16:03:00 +0200

    Update (no dumb spider)
    >:(

--- digdig01_grass/Assets/Scipts/spindel/spindelGroundCheck.cs
+++ digdig01_grass/Assets/Scipts/spindel/spindelGroundCheck.cs
@@ -17,31 +17,5 @@ public class spindelGroundCheck : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "ground_Tag")
-        {
-            Spider.GetComponent<spiderScript>().onTop = true;
-        }
-        /*else
-        {
-            Spider.GetComponent<spiderScript>().onTop = false; 
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.collider.tag != "ground_Tag")
-        {
-            Spider.GetComponent<spiderScript>().onTop = false;
-        }*/
-    }
-
-
-    /*private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "ground_tag")
-        {
-            Spider.GetComponent<spiderScript>().groundCheck = true; 
-        }
-    }*/
+   
 }
